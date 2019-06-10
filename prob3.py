7d6412240c6227b7d4f64db3e12bba96be424191
Cloud User <ec2-user@ip-172-31-38-46.ap-south-1.compute.internal>
2019-06-10 04:10:32 +0000

    Problem 3 python

---
+++ prob3.py
@@ -0,0 +1,18 @@
+
+ad=[0,1,2,4,6,3,6,7,9,10]
+x=[]
+y=[]
+print("Greater than 5")
+for i in ad:
+	if i>5:
+		print(i)
+		x.append(i)
+print("Less than equals to 2:")
+for i in ad :
+	if i <=2 :
+		print(i)
+		y.append(i)
+
+print("Greater than 5 list :",x)
+print("Less than or euals to 2 list :",y)
+
